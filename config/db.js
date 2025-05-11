267fef74ac87a2e20ed6ed6bab9c04054914c714
Sudhanshu jha <116909414+SudhanshuDTU@users.noreply.github.com>
2025-05-11 21:12:29 +0530

    Update db.js

--- config/db.js
+++ config/db.js
@@ -9,5 +9,8 @@ const pool = new Pool({
   database: process.env.DB_NAME,
   password: process.env.DB_PASSWORD,
   port: process.env.DB_PORT,
+  ssl: {
+    rejectUnauthorized: false // use this only for Render or dev environments
+  },
 })
 export default pool;
