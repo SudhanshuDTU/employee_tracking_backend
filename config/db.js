ba3398fc8d40cafbb7d6aa973a3076d3d41d3afa
Sudhanshu jha <116909414+SudhanshuDTU@users.noreply.github.com>
2025-07-12 10:34:23 +0530

    Update db.js

--- config/db.js
+++ config/db.js
@@ -9,5 +9,8 @@ const pool = new Pool({
   database: process.env.DB_NAME,
   password: process.env.DB_PASSWORD,
   port: process.env.DB_PORT,
+  ssl: {
+    rejectUnauthorized: false // This is crucial for Render's default self-signed certificates
+  }
 })
 export default pool;
